ae54579f87935773bc2acdcf57431bbf721335e0
Guillaume CATEL <hello@guillaumecatel.com>
2022-11-25 07:17:50 +0100

    chore: update storybook config

--- src/majora/majora.tsx
+++ src/majora/majora.tsx
@@ -17,12 +17,16 @@ const vertexShader = /* glsl */ `
 `
 
 const fragmentShader = /* glsl */ `
+
+  #ifdef GL_ES
+  precision mediump float;
+  #endif
+
   varying vec2 vUv;
   varying vec3 vPosition;
   uniform vec4 uResolution;
   uniform float uTime;
 
-  // noise
   float mod289(float x) {
     return x - floor(x * (1.0 / 289.0)) * 289.0;
   }
@@ -36,25 +40,25 @@ const fragmentShader = /* glsl */ `
   }
 
   float noise(vec3 p) {
-      vec3 a = floor(p);
-      vec3 d = p - a;
-      d = d * d * (3.0 - 2.0 * d);
+    vec3 a = floor(p);
+    vec3 d = p - a;
+    d = d * d * (3.0 - 2.0 * d);
 
-      vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
-      vec4 k1 = perm(b.xyxy);
-      vec4 k2 = perm(k1.xyxy + b.zzww);
+    vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+    vec4 k1 = perm(b.xyxy);
+    vec4 k2 = perm(k1.xyxy + b.zzww);
 
-      vec4 c = k2 + a.zzzz;
-      vec4 k3 = perm(c);
-      vec4 k4 = perm(c + 1.0);
+    vec4 c = k2 + a.zzzz;
+    vec4 k3 = perm(c);
+    vec4 k4 = perm(c + 1.0);
 
-      vec4 o1 = fract(k3 * (1.0 / 41.0));
-      vec4 o2 = fract(k4 * (1.0 / 41.0));
+    vec4 o1 = fract(k3 * (1.0 / 41.0));
+    vec4 o2 = fract(k4 * (1.0 / 41.0));
 
-      vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
-      vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+    vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+    vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
 
-      return o4.y * d.y + o4.x * (1.0 - d.y);
+    return o4.y * d.y + o4.x * (1.0 - d.y);
   }
 
   mat2 rotate2d(float angle) {
